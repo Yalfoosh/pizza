ad988a33c8fc477cc998f1279d056b16d0e21d27
Yalfoosh <headsouldev@gmail.com>
2020-10-20 02:23:15 +0200

    Comment code. Move demo to separate folder

--- src/yeast_prediction/preprocessing/dataset.py
+++ src/yeast_prediction/preprocessing/dataset.py
@@ -1,6 +1,6 @@
 import csv
 from pathlib import Path
-from typing import Union
+from typing import List, Union
 
 from .utils import fahrenheit_to_kelvin
 
@@ -8,7 +8,19 @@ from .utils import fahrenheit_to_kelvin
 def preprocess_raw_yeast_dataset(
     source_path: Union[Path, str],
     delimiter: str = "\t",
-):
+) -> List[List[Union[float, int]]]:
+    """
+    Preprocesses a dataset in the form of a CSV file.
+
+    Args:
+        source_path (Union[Path, str]): A Path or a str representing the file path of
+        the dataset you wish to preprocess.
+        delimiter (str, optional): The delimiter of the CSV file. Defaults to "\t".
+
+    Returns:
+        List[List[Union[float, int]]]: A list of rows that contained the preprocessed
+        data.
+    """
     new_rows = list()
     new_rows.append(
         ["temperature_kelvin", "cake_yeast_percentage", "fermentation_hours"]
--- src/yeast_prediction/preprocessing/utils.py
+++ src/yeast_prediction/preprocessing/utils.py
@@ -1,5 +1,17 @@
 from typing import Union
 
 
-def fahrenheit_to_kelvin(temperature_in_fahrenheit: Union[float, int]):
+def fahrenheit_to_kelvin(temperature_in_fahrenheit: Union[float, int]) -> float:
+    """
+    Converts a value from the Fahrenheit scale to the equivalent value on the Kelvin
+    scale.
+
+    Args:
+        temperature_in_fahrenheit (Union[float, int]): A float or int representing the
+        value of a temperature on the Fahrenheit scale you wish to convert to the
+        Kelvin scale.
+
+    Returns:
+        float: Argument temperature_in_fahrenheit converted to the Kelvin scale.
+    """
     return (temperature_in_fahrenheit + 459.67) * 5 / 9
